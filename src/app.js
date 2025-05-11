565e25f9c16066990f89f92598cef2a2e28c8df4
Jorge Sanchez Ortiz <jorgesoibz@gmail.com>
2025-05-11 08:50:54 +0000

    GenerateExcuse

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['Mi perro', 'Mi abuela', 'La dependienta', 'Mi perro'];
+let action = ['se comio', 'rompio', 'robo', 'canjeo'];
+let what = ['mis deberes', 'mi mobil', 'el articulo','el vaso'];
+let when = ['mientras conducia', 'cuando dormia', 'mientras estaba haciendo ejercicio', 'cuando comia', 'mientras rezaba'];
+
+function generateExcuses(){
+  let randomWho= who[Math.floor(Math.random()*who.length)];
+  let randomAction= action[Math.floor(Math.random()*action.length)];
+  let randomwhat= what[Math.floor(Math.random()*what.length)];
+  let randomwhen= when[Math.floor(Math.random()*when.length)];
+
+  return `${randomWho} ${randomAction} ${randomwhat} ${randomwhen}`;
+}
+document.getElementById('excuse').innerHTML= generateExcuses();
 };
